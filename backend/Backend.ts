073a511b3b40af080cb6d81c0590fcc618a5c2e0
Tom Modeste <tom.modeste@gmail.com>
2023-08-16 17:30:49 +0200

    added backend and config

---
+++ backend/Backend.ts
@@ -0,0 +1,54 @@
+export interface IComment {
+	id: number;
+	content: string;
+	date: number;
+	owner: number;
+	parent: number;
+}
+
+export interface IPost {
+	id: number;
+	date: number;
+	details: string;
+	owner: number;
+	title: string;
+}
+
+export interface IUser {
+	id: number;
+	name: string;
+	email: string;
+}
+
+export interface Backend {
+	// TODO
+	// améliorer/vérifier les null cases
+	// vérifier data
+	// vérifier omit des dates aussi ✔
+	// TODO
+
+	// comments
+	getAllComments: () => Promise<IComment[]>,
+	getOneComment:  (id: IComment['id']) => Promise<IComment>,
+	addComment:  (data: Omit<IComment, 'id' | 'date'>) => Promise<void>,
+	updateComment: (data: IComment) => Promise<void>,
+	deleteComment: (id: IComment['id']) => Promise<void>,
+
+	// posts
+	getAllPosts: () => Promise<IPost[]>,
+	getOnePost: (id: IPost['id']) => Promise<IPost>,
+	getPostComments: (id: IPost['id']) => Promise<IComment[] | null>,
+	addPost: (data: Omit<IPost, 'id' | 'date'>) => Promise<void>,
+	updatePost: (data: IPost) => Promise<void>,
+	deletePost: (id: IPost['id']) => Promise<void>,
+	getThread: (id: IPost['id']) => Promise<(IPost | IComment)[]>
+
+	// users
+	getAllUsers: () => Promise<IUser[]>,
+	getOneUser:(id: IUser['id']) => Promise<IUser>,
+	getUserComments: (id: IUser['id']) => Promise<IComment[] | null>
+	getUserPosts: (id: IUser['id']) => Promise<IPost[] | null>
+	addUser: (data: Omit<IUser, 'id'>) => Promise<void>,
+	updateUser: (data: IUser) => Promise<void>,
+	deleteUser: (id: IUser['id']) => Promise<void>,
+}
---
+++ backend/useBackend.ts
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import {useMemo} from "react";
+
+import {API_ENDPOINT} from '../config/config';
+import {Backend} from "./Backend";
+
+// import type {AxiosError, AxiosPromise, AxiosResponse} from 'axios';
+
+const backend = axios.create({
+	baseURL: API_ENDPOINT,
+});
+
+export const useBackend = () => {
+	return useMemo<Backend>(
+			() => ({
+				getAllComments: async () => backend.get('/comments/all'),
+				getOneComment:  async id => backend.get(`/comments/${id}`),
+				addComment:  async data => backend.post('/comments/add', {data}),
+				updateComment: async data => backend.put('/comments/update', {data}),
+				deleteComment: async id => backend.delete(`/comments/delete/${id}`),
+
+				// posts
+				getAllPosts: async () => backend.get('/posts/all'),
+				getOnePost: async id => backend.get(`/posts/${id}`),
+				addPost: async data => backend.post('/posts/add', {data}),
+				updatePost: async data  => backend.put('/posts/update', {data}),
+				deletePost: async id => backend.delete(`/posts/delete/${id}`),
+				getPostComments: async id => backend.get(`/posts/${id}/comments`),
+				getThread: async id => backend.get(`/posts/${id}/thread`),
+
+				// users
+				getAllUsers: async () => backend.get('/users/all'),
+				getOneUser: async id => backend.get(`/users/${id}`),
+				addUser: async data => backend.post('/users/add', {data}),
+				updateUser: async data => backend.put('/users/update', {data}),
+				deleteUser: async id => backend.delete(`/users/delete/${id}`),
+				getUserComments: async id => backend.get(`/users/${id}/comments`),
+				getUserPosts: async id => backend.get(`/users/${id}/posts`),
+			}),
+			[]
+	)
+}
